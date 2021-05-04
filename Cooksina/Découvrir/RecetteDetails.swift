266a6e34b16a3c12ba49327061fd2e34943f3708
Alexandre Dias Da Silva <alexandre.diasdasilva@yahoo.fr>
2021-05-04 18:45:41 +0200

    WIP

--- Cooksina/Découvrir/RecetteDetails.swift
+++ Cooksina/Découvrir/RecetteDetails.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct RecetteDetails: View {
-
+    
     @State var detailRecette = ""
     @State private var hideText : Bool = false
     
@@ -16,61 +16,97 @@ struct RecetteDetails: View {
     
     var body: some View {
         
-        VStack (alignment: .leading){
-                HStack {
-                    Image(systemName: "heart.fill")
-                        .padding(.leading)
-                    Text(recetteChoisie.titre)
-                        .font(.headline)
-                        .fontWeight(.medium)
-                        .padding(.horizontal)
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.leadinghalf.fill")
-                }
-            HStack(alignment: .center){
-                Spacer()
-                Text("\(recetteChoisie.region.rawValue) ")
-                Spacer()
-            }
-            Image(recetteChoisie.image)
+        ScrollView {
+            VStack {
+                
+                ZStack(alignment: .bottom) {
+                    Image(recetteChoisie.image)
                         .resizable()
                         .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(height: 250)
-                       .clipShape(Rectangle())
-            
-            if let utilisateur = recetteChoisie.createur() {
-                InfosUtilisateur(utilisateur: utilisateur)
-            } else {
-                Text(recetteChoisie.auteur)
-            }
-            
-            lesIcones(myrecetteChoisie: recetteChoisie)
+                        .frame(height: 350)
+                        .clipShape(Rectangle())
+                    
+                    Rectangle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .bottom, endPoint: .top))
+                        .frame(height: 100)
+                        .opacity(0.9)
+                    
+                    
+                    Text(recetteChoisie.titre)
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                
+                HStack(alignment: .center, spacing: 30) {
+                    HStack {
+                        Image(systemName: "globe")
+                            .foregroundColor(Color.gray)
+                        Text("\(recetteChoisie.region.rawValue) ")
+                            .font(.caption)
+                            .foregroundColor(Color.gray)
+                    }
+                    
+                    if let utilisateur = recetteChoisie.createur() {
+                        
+                        NavigationLink(destination: ProfilView()) {
+                            InfosUtilisateur(utilisateur: utilisateur)
+                        }
+                        
+                    } else {
+                        Text(recetteChoisie.auteur)
+                    }
+                    
+                    HStack {
+                        Image(systemName: "suit.heart.fill")
+                        Text("999")
+                    } .font(.caption)
+                    .foregroundColor(Color.red)
+                        
+                    
+                } .padding(5)
+                
+                Text("\(recetteChoisie.description)")
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                    .italic()
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Divider()
+                
+                lesIcones(myrecetteChoisie: recetteChoisie)
                 
                 VStack{
+                    
                     Picker(selection: $detailRecette, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/, content: {
-                        Text("Ingredients").tag("Ingredients")
-                        Text("Ustenciles").tag("Ustenciles")
+                        Text("Ingrédients").tag("Ingrédients")
+                        Text("Ustensiles").tag("Ustensiles")
                     })
                     .pickerStyle(SegmentedPickerStyle())
-                    
+                    .padding()
                     
                     if detailRecette == "Ustenciles" {
-                        matériel()
+                        Text("Remplacer par liste des ustensiles dynamique")
                     } else {
-                        Laliste()
+                        Text("Remplacer par liste des ustensiles dynamique")
+                    }
+                    
+                    Divider()
+                    
+                    Text(recetteChoisie.instructions)
+                        .padding()
+                    
                 }
-            }
-                Spacer()
-        } .navigationBarTitle("\(recetteChoisie.titre)", displayMode: .inline) 
-
-    
-        
+                
+            } .navigationBarTitle("\(recetteChoisie.titre)", displayMode: .inline)
+        }
     }
 } 
 
 struct RecetteDetails_Previews: PreviewProvider {
-
+    
     static var previews: some View {
         RecetteDetails(recetteChoisie: recettes[1])
     }
@@ -81,8 +117,9 @@ struct lesIcones: View {
     let myrecetteChoisie: Recette
     
     var body: some View {
-        HStack (spacing: 55){
+        HStack {
             Spacer()
+            
             VStack(alignment: .center){
                 Image("toque")
                     .resizable()
@@ -90,25 +127,26 @@ struct lesIcones: View {
                     .frame(width: 60, height: 30)
                 Text("\(myrecetteChoisie.difficulte.rawValue)")
                     .font(.caption)}
+            
+            Spacer()
+            
             VStack(alignment: .center){
                 Image("chrono")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 60, height: 30)
                 Text("\(myrecetteChoisie.temps) min").font(.caption)}
-//            VStack(alignment: .center){
-//                Image("marmite")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width: 60, height: 30)
-//                Text(myrecetteChoisie.temps).font(.caption)}
+            
+            Spacer()
+
             VStack(alignment: .center){
                 Image("groupes")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 30, height: 30)
-                Text("\(myrecetteChoisie.nbPersonnes)pers")
+                Text("\(myrecetteChoisie.nbPersonnes) pers")
                     .font(.caption)}
+            
             Spacer()
         }
     }
@@ -116,6 +154,9 @@ struct lesIcones: View {
 
 struct matériel: View {
     var body: some View {
+        
+        
+        
         HStack{
             Image("batteur")
                 .resizable()
@@ -135,17 +176,6 @@ struct matériel: View {
     }
 }
 
-struct Laliste: View {
-    var body: some View {
-        VStack (spacing: 10){
-            Text("- 250g de poulet")
-            Text("- 100cl de crème")
-            Text("-  Poivron rouge ")
-            Text("- Cumin, Curry")
-            Text("- 2  Oignons")
-        }
-    }
-}
 
 //
 //{
